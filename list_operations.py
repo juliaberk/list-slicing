b301abb65e3a7fb514a26a51213d4d20e9495fc9
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-05 13:01:54 -0800

    updated more functions

--- list_operations.py
+++ list_operations.py
@@ -84,7 +84,7 @@ def first_three(input_list):
 
     """
 
-    return []
+    return input_list[:3]
 
 
 def last_five(input_list):
@@ -97,7 +97,7 @@ def last_five(input_list):
 
     """
 
-    return []
+    return input_list[-5:]
 
 
 def middle(input_list):
@@ -110,7 +110,7 @@ def middle(input_list):
 
     """
 
-    return []
+    return input_list[2:-2]
 
 
 def inner_four(input_list):
@@ -123,7 +123,7 @@ def inner_four(input_list):
 
     """
 
-    return []
+    return input_list[2:6]
 
 
 def inner_four_end(input_list):
@@ -139,7 +139,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2]
 
 
 def replace_head(input_list):
@@ -153,9 +153,9 @@ def replace_head(input_list):
     True
 
     """
+    input_list[0] = 42
 
-    pass
-
+    
 
 def replace_third_and_last(input_list):
     """Replace third and last elements of input_list with 37 and return nothing.
@@ -168,8 +168,7 @@ def replace_third_and_last(input_list):
     True
 
     """
-
-    pass
+    input_list[2], input_list[-1] = 37, 37
 
 
 def replace_middle(input_list):
@@ -188,7 +187,8 @@ def replace_middle(input_list):
 
     """
 
-    pass
+    input_list[2] = 42
+    input_list[3:-2] = [37]
 
 
 def delete_third_and_seventh(input_list):
